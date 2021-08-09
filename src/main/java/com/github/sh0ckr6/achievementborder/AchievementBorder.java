2e744fb2210d2769765cf868930fdc9afbf47cd7
Kaleb Redpath <sh0ck.04@outlook.com>
2021-08-09 17:40:40 -0400

    Initial commit

---
+++ src/main/java/com/github/sh0ckr6/achievementborder/AchievementBorder.java
@@ -0,0 +1,17 @@
+package com.github.sh0ckr6.achievementborder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AchievementBorder extends JavaPlugin {
+  
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+    
+  }
+  
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
