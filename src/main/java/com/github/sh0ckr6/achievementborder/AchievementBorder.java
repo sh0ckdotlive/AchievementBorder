eb340146587c97f609c000dc1a8df84b9e7b7af4
Kaleb Redpath <sh0ck.04@outlook.com>
2021-08-10 02:31:55 -0400

    Complete first rendition of the AchievementBorder system
    whoops forgot to commit this as I go oh well

--- src/main/java/com/github/sh0ckr6/achievementborder/AchievementBorder.java
+++ src/main/java/com/github/sh0ckr6/achievementborder/AchievementBorder.java
@@ -2,12 +2,14 @@ package com.github.sh0ckr6.achievementborder;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.swing.border.Border;
+
 public final class AchievementBorder extends JavaPlugin {
   
   @Override
   public void onEnable() {
     // Plugin startup logic
-    
+    new BorderControl(this);
   }
   
   @Override
---
+++ src/main/java/com/github/sh0ckr6/achievementborder/BorderControl.java
@@ -0,0 +1,91 @@
+package com.github.sh0ckr6.achievementborder;
+
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.WorldBorder;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Iterator;
+import java.util.logging.Level;
+
+public class BorderControl implements Listener {
+  
+  private AchievementBorder plugin;
+  
+  public BorderControl(AchievementBorder plugin) {
+    this.plugin = plugin;
+    plugin.getServer().getPluginManager().registerEvents(this, plugin);
+  }
+  
+  @EventHandler
+  public void onAchievementUnlock(PlayerAdvancementDoneEvent event) {
+    if (isValidAdvancement(event.getAdvancement())) {
+      Bukkit.getLogger().log(Level.INFO, "tryhard (onAchievementUnlock)");
+      World world = Bukkit.getWorlds().get(0);
+      WorldBorder border = world.getWorldBorder();
+  
+      border.setSize(border.getSize() + 5, 1);
+    }
+  }
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    Bukkit.getLogger().log(Level.FINE, "who the fuck are you (onPlayerJoin)");
+    int completedAdvancements = getCompletedAdvancementsCount(event.getPlayer());
+    WorldBorder border = event.getPlayer().getWorld().getWorldBorder();
+    
+    if (completedAdvancements * 5 + 1 > border.getSize()) {
+      border.setSize(completedAdvancements * 5 + 1, 1);
+      for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+        player.sendMessage(ChatColor.GOLD + "A new player with more advancements than the highest player has joined, changing border...");
+      }
+    }
+  }
+  
+  @EventHandler
+  public void onPlayerLeave(PlayerQuitEvent event) {
+    WorldBorder border = event.getPlayer().getWorld().getWorldBorder();
+    if (!(getCompletedAdvancementsCount(event.getPlayer()) * 5 + 1 >= border.getSize())) return;
+    Bukkit.getLogger().log(Level.INFO, "hey fucker we need to close the border down");
+    
+    int highestAdvancementCount = 0;
+    for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+      if (player == event.getPlayer()) continue;
+      highestAdvancementCount = Math.max(highestAdvancementCount, getCompletedAdvancementsCount(player));
+      player.sendMessage(ChatColor.RED + "A player with more advancements than the highest player has left, changing border...");
+    }
+    
+    border.setSize(highestAdvancementCount * 5 + 1, 1);
+  }
+  
+  private int getCompletedAdvancementsCount(Player player) {
+    int advancementsCompleted = 0;
+    Iterator<Advancement> it = Bukkit.advancementIterator();
+    while (it.hasNext()) {
+      Advancement advancement = it.next();
+      if (player.getAdvancementProgress(advancement).isDone()) {
+        advancementsCompleted++;
+      }
+    }
+    
+    return advancementsCompleted;
+  }
+  
+  private boolean isValidAdvancement(Advancement advancement) {
+    String advancementKey = advancement.getKey().getKey();
+    return advancementKey.startsWith("story") ||
+           advancementKey.startsWith("nether") ||
+           advancementKey.startsWith("adventure") ||
+           advancementKey.startsWith("end") ||
+           advancementKey.startsWith("husbandry");
+  }
+}
