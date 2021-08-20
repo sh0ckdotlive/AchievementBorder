1207a1ab1d0d566451cd81bb820461423de67341
Kaleb Redpath <sh0ck.04@outlook.com>
2021-08-20 13:54:09 -0400

    Add in-game config editing
    This was a pain to make, but it's finally done

--- src/main/java/com/github/sh0ckr6/achievementborder/commands/ConfigCommand.java
+++ src/main/java/com/github/sh0ckr6/achievementborder/commands/ConfigCommand.java
@@ -48,8 +48,10 @@ public class ConfigCommand extends BaseCommand implements TabCompleter {
    */
   @Override
   protected boolean execute(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-    if (args[0].equalsIgnoreCase("reload")) {
-      handleReload(sender, Arrays.copyOfRange(args, 1, args.length));
+    switch (args[0]) {
+      case "reload" -> handleReload(sender, Arrays.copyOfRange(args, 1, args.length));
+      case "edit" -> handleEdit(sender, Arrays.copyOfRange(args, 1, args.length));
+      default -> sender.sendMessage(ChatColor.RED + "Option " + args[0] + " not found! Please check your spelling and try again!");
     }
     return true;
   }
@@ -103,5 +105,66 @@ public class ConfigCommand extends BaseCommand implements TabCompleter {
       sender.sendMessage(ChatColor.RED + "The requested configuration file could not be found. Check you spelling and try again!");
       sender.sendMessage(ChatColor.RED + "If this issue is repeating and you know it shouldn't, make a " + ChatColor.GOLD + "bug report" + ChatColor.RED + " at https://github.com/sh0ckR6/AchievementBorder/issues/new/choose.");
     }
+  }
+  
+  private void handleEdit(CommandSender sender, String[] args) {
+    // Check if the user has provided arguments
+    if (args.length < 3) {
+      sender.sendMessage(ChatColor.RED + "You have provided too little arguments! Check your usage and try again!");
+      return;
+    }
+    if (args.length > 3) {
+      sender.sendMessage(ChatColor.RED + "You have provided too many arguments! Check your usage and try again!");
+      return;
+    }
+    
+    String config = args[0];
+    String path = args[1];
+    String value = args[2];
+  
+    Object oldValue = ConfigManager.readFromConfig(config, path);
+    
+    // Check if the value we are trying to edit can be edited This must be done because all type information is lost at
+    // runtime, which makes editing the files in-game annoyingly difficult.
+    if (!(oldValue instanceof Integer) && !(oldValue instanceof String) && !(oldValue instanceof Float) && !(oldValue instanceof Boolean)) {
+      sender.sendMessage(ChatColor.RED + "The type of value you are trying to edit is not yet supported! If you would like to see support added for this value, "
+                         + ChatColor.BOLD + "please make a feature request on the issue tracker: "
+                         + ChatColor.RESET + ChatColor.GOLD + "https://github.com/sh0ckR6/AchievementBorder/issues/new/choose");
+      return;
+    }
+    
+    // Convert the provided String value to the required type and set it in the config
+    // This could be made much cleaner with JDK 17's "Switch Pattern Matching" but this is JDK 16, not 17.
+    try {
+      if (oldValue instanceof Integer) {
+        ConfigManager.setInConfig(config, path, Integer.parseInt(value));
+      } else if (oldValue instanceof String) {
+        ConfigManager.setInConfig(config, path, value);
+      } else if (oldValue instanceof Float) {
+        ConfigManager.setInConfig(config, path, Float.parseFloat(value));
+      } else if (oldValue instanceof Boolean) {
+        // Check that the provided values are either true or false. The official implementation of Boolean.parseBoolean()
+        // only checks if the string is true, not if it's true or false. This means that any string other than true
+        // is considered to be false.
+        if (!value.equalsIgnoreCase("true") && !value.equalsIgnoreCase("false")) {
+          sender.sendMessage(ChatColor.RED + "The value you have provided is invalid! Please check your provided value and try again!");
+          return;
+        }
+        ConfigManager.setInConfig(config, path, Boolean.parseBoolean(value));
+      }
+    } catch (MissingResourceException e) {
+      sender.sendMessage(ChatColor.RED + "The configuration you have requested is non-existent! Check your spelling and try again!");
+      return;
+    } catch (NumberFormatException e) {
+      sender.sendMessage(ChatColor.RED + "The value you have provided is invalid! Please check your provided value and try again!");
+      return;
+    }
+  
+    sender.sendMessage(ChatColor.GREEN + "Successfully updated configuration "
+                       + ChatColor.GOLD + config
+                       + ChatColor.GREEN + " at path "
+                       + ChatColor.GOLD + path
+                       + ChatColor.GREEN + "! New value: "
+                       + ChatColor.GOLD + value);
   }
 }
